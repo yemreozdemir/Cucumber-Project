d2aef15a8db80e384cc98fcb9882388ea8364a3e
YasinKaradag07 <yasinkaradag3607@gmail.com>
2022-06-15 22:24:51 +0300

    OrderPage extended BasePage

--- src/test/java/com/cydeo/pages/OrderPage.java
+++ src/test/java/com/cydeo/pages/OrderPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class OrderPage {
+public class OrderPage extends BasePage{
 
     public OrderPage(){
         PageFactory.initElements(Driver.getDriver(),this);
