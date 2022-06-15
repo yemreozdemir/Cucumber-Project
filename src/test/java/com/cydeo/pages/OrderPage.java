5c8284e947d853a140893290a217f8aeebb85c96
YasinKaradag07 <yasinkaradag3607@gmail.com>
2022-06-15 22:22:28 +0300

    Order page eample created and half implemented

---
+++ src/test/java/com/cydeo/pages/OrderPage.java
@@ -0,0 +1,37 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class OrderPage {
+
+    public OrderPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(name = "product")
+    public WebElement productDropdown;
+
+    @FindBy(name = "quantityt")
+    public WebElement inputQuantity;
+
+    @FindBy(name = "name")
+    public WebElement inputName;
+
+    @FindBy(name = "street")
+    public WebElement inputStreet;
+
+    @FindBy(name = "city")
+    public WebElement inputCity;
+
+    @FindBy(name = "state")
+    public WebElement inputState;
+
+    @FindBy(name = "zip")
+    public WebElement inputZip;
+
+
+
+}
